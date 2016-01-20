7a56b4f16ba2e19b405fb71a75bcd781c0af57b6
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-01-20 11:36:11 +0100

    First version

---
+++ OrderCollector/src/main/java/se/lanteam/OrderCollectorApplication.java
@@ -0,0 +1,12 @@
+package se.lanteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCollectorApplication.class, args);
+	}
+}
