d4f72b91c123dc4bf362bc84d156c2f51b35e7a0
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-01-20 11:22:40 +0100

    First version

---
+++ DeliveryManagerGUI/src/main/java/se/lanteam/DeliveryManagerGuiApplication.java
@@ -0,0 +1,12 @@
+package se.lanteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagerGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryManagerGuiApplication.class, args);
+	}
+}
