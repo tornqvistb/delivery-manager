dcc3d3cf5d853f0fe18b03b28158657a78f41cb9
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-01-20 11:29:26 +0100

    First version

---
+++ MailReceiver/src/main/java/se/lanteam/MailReceiverApplication.java
@@ -0,0 +1,12 @@
+package se.lanteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailReceiverApplication.class, args);
+	}
+}
---
+++ MailReceiver/src/test/java/se/lanteam/MailReceiverApplicationTests.java
@@ -0,0 +1,18 @@
+package se.lanteam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MailReceiverApplication.class)
+@WebAppConfiguration
+public class MailReceiverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
