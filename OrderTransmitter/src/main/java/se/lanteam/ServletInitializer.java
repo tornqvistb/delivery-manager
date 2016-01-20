3dc782711207a10195709b8e998058783e2d8aee
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-01-20 11:33:51 +0100

    First version

---
+++ OrderTransmitter/src/main/java/se/lanteam/ServletInitializer.java
@@ -0,0 +1,13 @@
+package se.lanteam;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OrderTransmitterApplication.class);
+	}
+
+}
