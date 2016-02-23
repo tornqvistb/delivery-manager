29b31b4af3859a4a164357a1f1ad002f2fd91be8
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-02-23 13:03:32 +0100

    Some changes regarding deployment

--- DeliveryManagerGUI/src/main/java/se/lanteam/DeliveryManagerGuiApplication.java
+++ DeliveryManagerGUI/src/main/java/se/lanteam/DeliveryManagerGuiApplication.java
@@ -2,11 +2,17 @@ package se.lanteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeliveryManagerGuiApplication {
+public class DeliveryManagerGuiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryManagerGuiApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	         return application.sources(DeliveryManagerGuiApplication.class);
 	}
 }
