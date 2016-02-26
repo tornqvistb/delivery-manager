0747910628c3b9e40deaefc5dc16fa51407f0fca
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2016-02-26 09:38:50 +0100

    Added reconnect=true on database connection strings

--- build.sh
+++ build.sh
@@ -11,3 +11,8 @@ mvn clean package -DskipTests
 cd ../OrderTransmitter
 mvn clean package -DskipTests 
 cd ..
+cp DeliveryManagerGUI/target/DeliveryManagerGUI*.war ../release/DeliveryManagerGUI.war
+cp MailReceiver/target/MailReceiver*.war ../release/MailReceiver.war
+cp OrderCollector/target/OrderCollector*.war ../release/OrderCollector.war
+cp OrderResponseReceiver/target/OrderResponseReceiver*.war ../release/OrderResponseReceiver.war
+cp OrderTransmitter/target/OrderTransmitter*.war ../release/OrderTransmitter.war
