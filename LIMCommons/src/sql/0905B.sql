7961060666bcf21baaa2ff0c1e8b22e5c70869cd
Björn Törnqvist <bjorn.tornqvist@intraservice.goteborg.se>
2017-09-06 22:54:50 +0200

    Script to add auto increment on keys in some tables.

--- LIMCommons/src/sql/0905B.sql
+++ LIMCommons/src/sql/0905B.sql
@@ -2,7 +2,7 @@ SET FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE customer_group MODIFY COLUMN id BIGINT(20) auto_increment;
 ALTER TABLE customer_custom_field MODIFY COLUMN id BIGINT(20) auto_increment;
-ALTER TABLE registration_config MODIFY COLUMN document_id INT auto_increment;
-ALTER TABLE reports_config MODIFY COLUMN document_id INT auto_increment;
+ALTER TABLE registration_config MODIFY COLUMN id BIGINT(20) auto_increment;
+ALTER TABLE reports_config MODIFY COLUMN id BIGINT(20) auto_increment;
 
 SET FOREIGN_KEY_CHECKS=1;
