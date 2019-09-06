a7288ab71adbf51d90364398c41c810dda3c3fa1
Björn Törnqvist <tornqvistb@gmail.com>
2019-09-06 06:22:27 +0200

    Rensning av gamla db properties.

--- LIMCommons/src/sql/201908.2.sql
+++ LIMCommons/src/sql/201908.2.sql
@@ -1,1 +1,2 @@
-insert into system_property (id, string_value, number_value) values ('max-orders-in-search', 'Max antal ordrar i träfflista vid sökning', 50);
+insert into system_property (id, string_value, number_value) values ('max-orders-in-search', 'Max antal ordrar i träfflista vid sökning', 500);
+delete from system_property where id = 'max-days-inactive-orders-search';
