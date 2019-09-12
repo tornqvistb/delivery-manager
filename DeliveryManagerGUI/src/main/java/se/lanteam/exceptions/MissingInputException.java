8095d9564a341afc41280049d895cbb84f4f79e8
Björn Törnqvist <tornqvistb@gmail.com>
2019-09-12 09:35:57 +0200

    Ny exceptionklass för saknad indata.

---
+++ DeliveryManagerGUI/src/main/java/se/lanteam/exceptions/MissingInputException.java
@@ -0,0 +1,14 @@
+package se.lanteam.exceptions;
+
+/**
+ * MissingInputException
+ *
+ */
+public class MissingInputException extends Exception {
+	private static final long serialVersionUID = 5078622028197805638L;
+	
+	public MissingInputException(String message) {
+		super(message);
+	}
+
+}
