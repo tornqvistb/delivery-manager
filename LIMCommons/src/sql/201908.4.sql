52c11695de6b8dee426d91155750a7b0855556c8
Björn Törnqvist <tornqvistb@gmail.com>
2019-09-20 14:46:56 +0200

    Skapat SQL-script

--- LIMCommons/src/sql/201908.4.sql
+++ LIMCommons/src/sql/201908.4.sql
@@ -1,2 +1,4 @@
 update customer_group set allow_pre_delivery_info = b'0';
 update customer_group set allow_pre_delivery_info = b'1' where id = 1;
+update order_header set transfer_date = delivery_date;
+alter table order_header modify article_numbers VARCHAR(10000);
